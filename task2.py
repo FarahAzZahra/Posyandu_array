c8885ccdc55ee4d27c7006a26c8f7431d855544b
FarahAzZahra <35552733+FarahAzZahra@users.noreply.github.com>
2020-11-16 20:41:39 +0700

    update -error

--- task2.py
+++ task2.py
@@ -4,3 +4,8 @@ print("Klub A : ", end="")
 A = input()
 print("Klub B : ", end="")
 B = input()
+
+n = input().split()
+arrTanding.insert(0,n)
+i = 0
+while (arrTanding[i][0] > 0) and (arrTanding[i][1] > 0):
