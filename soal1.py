5fa08b2ce8f2e927e3f3370d572d3e1f9f2c586e
FarahAzZahra <35552733+FarahAzZahra@users.noreply.github.com>
2020-11-16 20:22:03 +0700

    Complete task

--- soal1.py
+++ soal1.py
@@ -5,15 +5,22 @@ bMax = 0
 def hitungMinMax(arrBerat):
     global bMin, bMax
     # Definisikan Proses Mencari Berat Maximum Dan Minimum
-
+    bMin = arrBerat[0]
+    for n in arrBerat:
+        if bMax < n :
+            bMax = n
+        if bMin > n :
+            bMin = n
 
 def rerata(arrBerat):
     total = 0
-
+    for n in arrBerat:
+        total = total + n
     # Definisikan Proses Mencari Rerata Dari Total Berat
+    rerata = total / len(arrBerat)
 
     # Return Hasil Rerata
-
+    return rerata
 
 print('Masukkan Banyak Data Berat Balita :', end=" ")
 n = int(input())
@@ -21,11 +28,14 @@ n = int(input())
 for i in range(n):
     print(f'Masukkan Berat Balita Ke-{i+1} :', end=' ')
     # Inisialisasi Input Data Berat
-
+    data = input()
     # Masukkan Data Berat Ke Array (arrBerat)
-
+    arrBerat.insert(i,float(data))
 
 # Panggil procedur hitungMinMax(arrBerat)
-
+hitungMinMax(arrBerat)
 
 # Print Data Minimum, Maximum, dan Rerata Berat
+print("Berat Balita Minimum : {0:.2f} kg".format(bMin))
+print("Berat Balita Maksimum : {0:.2f} kg".format(bMax))
+print("Rerata berat balita : ",round(rerata(arrBerat), 2)," kg")
