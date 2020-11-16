13c1a4853740c2a84baf8219355add131e48dff0
FarahAzZahra <35552733+FarahAzZahra@users.noreply.github.com>
2020-11-16 20:19:52 +0700

    First Commit
    Soal dari tugas

---
+++ soal1.py
@@ -0,0 +1,31 @@
+arrBerat = []
+bMin = 0
+bMax = 0
+
+def hitungMinMax(arrBerat):
+    global bMin, bMax
+    # Definisikan Proses Mencari Berat Maximum Dan Minimum
+
+
+def rerata(arrBerat):
+    total = 0
+
+    # Definisikan Proses Mencari Rerata Dari Total Berat
+
+    # Return Hasil Rerata
+
+
+print('Masukkan Banyak Data Berat Balita :', end=" ")
+n = int(input())
+
+for i in range(n):
+    print(f'Masukkan Berat Balita Ke-{i+1} :', end=' ')
+    # Inisialisasi Input Data Berat
+
+    # Masukkan Data Berat Ke Array (arrBerat)
+
+
+# Panggil procedur hitungMinMax(arrBerat)
+
+
+# Print Data Minimum, Maximum, dan Rerata Berat
