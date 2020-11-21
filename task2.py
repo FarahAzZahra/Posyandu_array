c62b5b3058969c64f5d02dec7730a26a0a4a7e99
FarahAzZahra <35552733+FarahAzZahra@users.noreply.github.com>
2020-11-21 18:50:14 +0700

    Finishing
    :)

--- task2.py
+++ task2.py
@@ -5,7 +5,23 @@ A = input()
 print("Klub B : ", end="")
 B = input()
 
-n = input().split()
-arrTanding.insert(0,n)
-i = 0
-while (arrTanding[i][0] > 0) and (arrTanding[i][1] > 0):
+print("Pertandingan 1 : ", end="")
+a = input().split()
+arrTanding.append(a)
+i=0
+while int(arrTanding[i][0])>=0 and int(arrTanding[i][1])>=0:
+    print("Pertandingan {} : ".format(i+2), end="")
+    a = input().split()
+    arrTanding.append(a)
+    i = i+1
+
+j=0
+while j < i:
+    if (int(arrTanding[j][0]) > int(arrTanding[j][1])) :
+        print("Hasil {} : {}".format(j+1,A))
+    elif (int(arrTanding[j][0])<int(arrTanding[j][1])) :
+        print("Hasil {} : {}".format(j+1,B))
+    else :
+        print("Hasil {} : Draw".format(j+1))
+    j = j+1
+print("Pertandingan selesai")
